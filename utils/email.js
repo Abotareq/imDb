05becfb46deffc8a30165ed982d4f7ca0303d77b
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-10-04 01:09:46 +0300

    email

--- utils/email.js
+++ utils/email.js
@@ -10,9 +10,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-/*   tls: {
+  tls: {
     rejectUnauthorized: false, 
-  }, */
+  },
 });
 
 // Function to replace template placeholders with actual data
