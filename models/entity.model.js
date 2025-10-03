e6170048ee7997a1f575be2572522a5c59f436d9
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-10-04 01:09:13 +0300

    DROPPED INDEX

--- models/entity.model.js
+++ models/entity.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const genreSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true, trim: true },
+  name: { type: String, required: true,/*  unique: true, */ trim: true },
   description: String
 }, { timestamps: true });
 
